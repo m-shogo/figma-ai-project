9af5b2264a853e4cd50fe2d687d693c29aa2cac7
m-shogo <60862647+m-shogo@users.noreply.github.com>
2026-08-11 13:28:42 +0900

    feat: enqueue Links visual First Pass styles

--- experiments/ref001-wordpress-acf/fixture-theme/functions.php
+++ experiments/ref001-wordpress-acf/fixture-theme/functions.php
@@ -22,21 +22,28 @@ function ref001_learning_enqueue_assets() {
 		'ref001-learning',
 		get_theme_file_uri( 'assets/css/ref001.css' ),
 		array(),
-		'0.3.0'
+		'0.4.0'
 	);
 
 	wp_enqueue_style(
 		'ref001-learning-education',
 		get_theme_file_uri( 'assets/css/ref001-education.css' ),
 		array( 'ref001-learning' ),
-		'0.3.0'
+		'0.4.0'
 	);
 
 	wp_enqueue_style(
 		'ref001-learning-courses',
 		get_theme_file_uri( 'assets/css/ref001-courses.css' ),
 		array( 'ref001-learning' ),
-		'0.3.0'
+		'0.4.0'
+	);
+
+	wp_enqueue_style(
+		'ref001-learning-links',
+		get_theme_file_uri( 'assets/css/ref001-links.css' ),
+		array( 'ref001-learning' ),
+		'0.4.0'
 	);
 }
 add_action( 'wp_enqueue_scripts', 'ref001_learning_enqueue_assets' );
