ed3b1172b7826b79cf9406868e2ed7f754b8cc6c
m-shogo <60862647+m-shogo@users.noreply.github.com>
2026-08-11 12:38:10 +0900

    Add Education to REF-001 Page fixture

--- experiments/ref001-wordpress-acf/fixture-theme/page-templates/template-ref001.php
+++ experiments/ref001-wordpress-acf/fixture-theme/page-templates/template-ref001.php
@@ -16,6 +16,7 @@ get_header();
 <main id="primary" class="ref001-page" data-reference-id="REF-001-CHIBA-KEIZAI-SAMPLE">
 	<?php get_template_part( 'template-parts/ref001/main-visual' ); ?>
 	<?php get_template_part( 'template-parts/ref001/reason' ); ?>
+	<?php get_template_part( 'template-parts/ref001/education' ); ?>
 </main>
 <?php
 get_footer();
