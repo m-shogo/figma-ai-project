3c778b79e0b8ef755a8aabe28c8590f0be76c9fe
m-shogo <60862647+m-shogo@users.noreply.github.com>
2026-08-11 13:34:00 +0900

    feat: enqueue Footer visual First Pass styles

--- experiments/ref001-wordpress-acf/fixture-theme/functions.php
+++ experiments/ref001-wordpress-acf/fixture-theme/functions.php
@@ -22,35 +22,42 @@ function ref001_learning_enqueue_assets() {
 		'ref001-learning',
 		get_theme_file_uri( 'assets/css/ref001.css' ),
 		array(),
-		'0.5.0'
+		'0.6.0'
 	);
 
 	wp_enqueue_style(
 		'ref001-learning-education',
 		get_theme_file_uri( 'assets/css/ref001-education.css' ),
 		array( 'ref001-learning' ),
-		'0.5.0'
+		'0.6.0'
 	);
 
 	wp_enqueue_style(
 		'ref001-learning-courses',
 		get_theme_file_uri( 'assets/css/ref001-courses.css' ),
 		array( 'ref001-learning' ),
-		'0.5.0'
+		'0.6.0'
 	);
 
 	wp_enqueue_style(
 		'ref001-learning-links',
 		get_theme_file_uri( 'assets/css/ref001-links.css' ),
 		array( 'ref001-learning' ),
-		'0.5.0'
+		'0.6.0'
 	);
 
 	wp_enqueue_style(
 		'ref001-learning-cta-value',
 		get_theme_file_uri( 'assets/css/ref001-cta-value.css' ),
 		array( 'ref001-learning' ),
-		'0.5.0'
+		'0.6.0'
+	);
+
+	wp_enqueue_style(
+		'ref001-learning-footer',
+		get_theme_file_uri( 'assets/css/ref001-footer.css' ),
+		array( 'ref001-learning' ),
+		'0.6.0'
 	);
 }
 add_action( 'wp_enqueue_scripts', 'ref001_learning_enqueue_assets' );
