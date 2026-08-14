8f4e51dfa678dc264d1b88d1d9816a13f16ea9ea
m-shogo <60862647+m-shogo@users.noreply.github.com>
2026-08-14 17:54:41 +0900

    REF-001: use outlined SVG logo in header

--- experiments/ref001-blind-clean-20260812/implementation/theme/template-parts/header-site.php
+++ experiments/ref001-blind-clean-20260812/implementation/theme/template-parts/header-site.php
@@ -1,12 +1,8 @@
 <?php $request_url = ref001_link_url('document'); $open_campus_url = ref001_link_url('open-campus'); ?>
 <header class="ref-header" data-section="header">
     <div class="ref-header__inner">
-        <div class="ref-header-logo" aria-label="千葉経済大学 CHIBA KEIZAI">
-            <img class="ref-header-logo__mark" src="<?php ref001_e(ref001_asset_url('assets/icons/footer-logo-mark.svg')); ?>" alt="">
-            <div class="ref-header-logo__type">
-                <span class="ref-header-logo__jp">千葉経済大学</span>
-                <span class="ref-header-logo__en">CHIBA KEIZAI</span>
-            </div>
+        <div class="ref-header-logo">
+            <img class="ref-header-logo__image" src="<?php ref001_e(ref001_asset_url('assets/icons/university-logo-outlined.svg')); ?>" alt="千葉経済大学 CHIBA KEIZAI">
         </div>
         <nav class="ref-header__actions" aria-label="関連アクション">
             <a href="<?php ref001_e($request_url); ?>" class="ref-action ref-action--blue" data-link-status="UNRESOLVED"><?php ref001_icon('document', 'ref-action__icon'); ?><span>資料請求</span></a>
