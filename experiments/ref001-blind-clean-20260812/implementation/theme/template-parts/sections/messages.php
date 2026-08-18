82f8e9c2bc301d6fa093ff56eadffdbd7f22c990
m-shogo <60862647+m-shogo@users.noreply.github.com>
2026-08-18 17:57:09 +0900

    REF-001: slide dummy images with Messages copy

--- experiments/ref001-blind-clean-20260812/implementation/theme/template-parts/sections/messages.php
+++ experiments/ref001-blind-clean-20260812/implementation/theme/template-parts/sections/messages.php
@@ -1,21 +1,25 @@
 <?php
 $slides = [
     [
+        'image' => 'messages-photo',
         'pc' => ['大学で培った企画力を武器に、', '今はIT企業のマーケターとして挑戦の毎日です！'],
         'sp' => ['大学で培った企画力を武器に、', '今はIT企業のマーケターとして', '挑戦の毎日です！'],
         'profile' => ref001_get('message_profile'), 'school' => ref001_get('message_school'),
     ],
     [
+        'image' => 'reason-1',
         'pc' => ['ゼミで身につけた行動力を活かして、', '地域と企業をつなぐ仕事に挑戦しています！'],
         'sp' => ['ゼミで身につけた行動力を活かして、', '地域と企業をつなぐ仕事に', '挑戦しています！'],
         'profile' => ref001_get('message_2_profile'), 'school' => ref001_get('message_2_school'),
     ],
     [
+        'image' => 'reason-2',
         'pc' => ['数字と向き合う力が自信になり、', '会計の知識を活かせる進路が見えてきました！'],
         'sp' => ['数字と向き合う力が自信になり、', '会計の知識を活かせる進路が', '見えてきました！'],
         'profile' => ref001_get('message_3_profile'), 'school' => ref001_get('message_3_school'),
     ],
     [
+        'image' => 'reason-3',
         'pc' => ['先生や仲間と考え抜いた経験を糧に、', '自分らしい働き方を目指しています！'],
         'sp' => ['先生や仲間と考え抜いた経験を糧に、', '自分らしい働き方を', '目指しています！'],
         'profile' => ref001_get('message_4_profile'), 'school' => ref001_get('message_4_school'),
@@ -28,7 +32,6 @@ $slides = [
         <h2><span>力をつけ活躍する</span><strong>先輩たち</strong></h2>
     </header>
     <div class="swiper ref-messages__swiper" data-ref-messages-swiper>
-        <?php ref001_picture('messages-photo', 'ref-messages__photo ref-messages__photo--static', '', true); ?>
         <div class="swiper-wrapper">
             <?php foreach ($slides as $index => $slide): ?>
                 <div class="swiper-slide ref-messages__slide" data-message-slide="<?= $index + 1; ?>">
@@ -38,6 +41,7 @@ $slides = [
                             <p class="ref-messages__quote ref-messages__quote--sp"><?php foreach ($slide['sp'] as $line): ?><span><?php ref001_e($line); ?></span><?php endforeach; ?></p>
                             <p class="ref-messages__profile"><?php ref001_e($slide['profile']); ?><br><?php ref001_e($slide['school']); ?></p>
                         </div>
+                        <?php ref001_picture($slide['image'], 'ref-messages__photo ref-messages__photo--slide', '', $index === 0); ?>
                     </div>
                 </div>
             <?php endforeach; ?>
