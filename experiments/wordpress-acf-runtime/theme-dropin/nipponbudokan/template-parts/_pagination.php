a6efae42bebfc8cfbe3a96c592b1ed66c62578b1
m-shogo <60862647+m-shogo@users.noreply.github.com>
2026-08-28 21:41:48 +0900

    Budokan: add news-specific pager composition

--- experiments/wordpress-acf-runtime/theme-dropin/nipponbudokan/template-parts/_pagination.php
+++ experiments/wordpress-acf-runtime/theme-dropin/nipponbudokan/template-parts/_pagination.php
@@ -1,30 +1,60 @@
 <?php
 global $wp_query;
 $props = isset($args) && isset($args['props']) ? $args['props'] : '';
+$variant = isset($args) && isset($args['variant']) ? sanitize_key($args['variant']) : '';
 $max_num_pages = $wp_query->max_num_pages;
 $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+
+$pagenate_args = array(
+    'total'              => $max_num_pages,
+    'current'            => $paged,
+    'show_all'           => false,
+    'end_size'           => 1,
+    'mid_size'           => 0,
+    'prev_next'          => true,
+    'prev_text'          => '<span></span>',
+    'next_text'          => '<span></span>',
+    'type'               => $variant === 'news' ? 'array' : 'plain',
+    'add_args'           => false,
+    'add_fragment'       => '',
+    'before_page_number' => '',
+    'after_page_number'  => '',
+);
 ?>
-<div class="module_pager-01">
+<?php if ($variant === 'news'): ?>
     <?php
-    $pagenate_args = array(
-        'total'              => $max_num_pages,
-        'current'            => $paged,
-        'show_all'           => false,
-        'end_size'           => 1,
-        'mid_size'           => 0,
-        'prev_next'          => true,
-        'prev_text'          => '<span></span>',
-        'next_text'          => '<span></span>',
-        'type'               => 'plain',
-        'add_args'           => false,
-        'add_fragment'       => '',
-        'before_page_number' => '',
-        'after_page_number'  => ''
-    );
+    $number_links = array();
+    $prev_link = '';
+    $next_link = '';
+
+    if ($max_num_pages == 1) {
+        $number_links[] = '<span aria-current="page" class="page-numbers current">1</span>';
+    } else {
+        $links = paginate_links($pagenate_args);
+        if (is_array($links)) {
+            foreach ($links as $link) {
+                if (strpos($link, 'prev page-numbers') !== false) {
+                    $prev_link = $link;
+                } elseif (strpos($link, 'next page-numbers') !== false) {
+                    $next_link = $link;
+                } else {
+                    $number_links[] = $link;
+                }
+            }
+        }
+    }
     ?>
-    <?php if ($max_num_pages == 1) : ?>
-        <span aria-current="page" class="page-numbers current">1</span>
-    <?php else : ?>
-        <?php echo paginate_links($pagenate_args); ?>
-    <?php endif; ?>
-</div>
+    <div class="module_pager-01 news_pager">
+        <div class="news_pager_prev"><?php echo $prev_link; ?></div>
+        <div class="news_pager_numbers"><?php echo implode('', $number_links); ?></div>
+        <div class="news_pager_next"><?php echo $next_link; ?></div>
+    </div>
+<?php else: ?>
+    <div class="module_pager-01">
+        <?php if ($max_num_pages == 1) : ?>
+            <span aria-current="page" class="page-numbers current">1</span>
+        <?php else : ?>
+            <?php echo paginate_links($pagenate_args); ?>
+        <?php endif; ?>
+    </div>
+<?php endif; ?>
