50a8a376667cd0a552230c2b5b7b37d0026b2b96
m-shogo <60862647+m-shogo@users.noreply.github.com>
2026-09-05 23:56:58 +0900

    fix(budokan): synchronize dropdown expanded semantics

--- experiments/wordpress-acf-runtime/theme-dropin/nipponbudokan/js/common.js
+++ experiments/wordpress-acf-runtime/theme-dropin/nipponbudokan/js/common.js
@@ -189,12 +189,20 @@ window.addEventListener('resize', setVw);
     dropdownNavItems.forEach(item => {
         const itemButton = item.querySelector('[class*="mdd_button"]');
         if (!itemButton) return;
+        const syncExpanded = function () {
+          itemButton.setAttribute(
+            'aria-expanded',
+            item.getAttribute('data-open') === 'true' ? 'true' : 'false',
+          );
+        };
+        syncExpanded();
         itemButton.addEventListener('click', function () {
             if (item.getAttribute('data-open') === 'true') {
                 item.setAttribute('data-open', 'false');
             } else {
                 item.setAttribute('data-open', 'true');
             }
+            syncExpanded();
         });
     });
   };
@@ -233,7 +241,7 @@ window.addEventListener('resize', setVw);
 
 //     // ナビ内リンクを抽出して正規化
 //     const getNavLinks = () =>
-//       $('a', $nav)
+//       $('a', nav)
 //         .toArray()
 //         .map((el) => {
 //           const $link = $(el);
@@ -596,7 +604,7 @@ window.addEventListener('resize', setVw);
     // );
     // // アーカイブナビゲーション
     // $('.an_links-01 > li > .anl_title').on('click', function () {
-    //   $(this).next().toggleClass('_open').slideToggle(400);
+    //   $(this).next().toggleClass('_open').slideToggle(300);
     //   $(this).toggleClass('_open');
     // });
     // // アーカイブナビゲーション枠外クリック時に閉じる
