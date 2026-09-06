de59c67a4a7d597d5aca011594eb7bc15e092a45
m-shogo <60862647+m-shogo@users.noreply.github.com>
2026-09-06 13:56:57 +0900

    fix(budokan): align explicit one-column content rhythm

--- experiments/wordpress-acf-runtime/theme-dropin/nipponbudokan/templates/template-oneColumn.php
+++ experiments/wordpress-acf-runtime/theme-dropin/nipponbudokan/templates/template-oneColumn.php
@@ -10,7 +10,7 @@ global $post;
     <?php if (!post_password_required($post->ID)) :  ?>
         <section>
             <?php get_template_part('template-parts/_visual'); ?>
-            <div class="global_inner _content">
+            <div class="global_inner _content _normalPage">
                 <div class="gc_main _oneColumn">
                     <div class="block-editor_wrap">
                         <?php the_content(); ?>
